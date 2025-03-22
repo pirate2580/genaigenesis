4305d87de0ed09b52f97b4314cb0048e8945e45c
pirate2580 <90294883+pirate2580@users.noreply.github.com>
2025-03-21 23:06:02 -0400

    got tailwind working

--- chrome-extension/src/App.tsx
+++ chrome-extension/src/App.tsx
@@ -10,11 +10,13 @@
 //   )
 // }
 
+import './index.css'
+
 function App() {
   return (
     <>
       <h1 className="text-blue-500 text-3xl font-bold underline">
-        This should be blue, large, bold, and underlined
+        This should be blue, large, bold, and underlined yeah
       </h1>
       <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
         Tailwind Button
